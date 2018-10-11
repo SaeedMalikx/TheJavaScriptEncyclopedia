b1b89a1b84f59ce5dc9889670b3d697873d81b6a
SaeedMalikx <saeedmalik360@gmail.com>
2018-10-10 21:58:17 -0400

    Async create chapters
    it should create chapters and added gitignore so there is no accid upload  of html files.
    
    -fix layout in object

--- make_book.js
+++ make_book.js
@@ -11,19 +11,35 @@ fs.readFile("./book.cyc", 'utf8', (err, data) => {
     let dataNew = dataString.slice(dataIncludeNum, -1)
     let dataList = dataNew.replace(/['"]+/g, '').replace(/@include+/g, '').replace(/\n+/g, '').replace(/\r+/g, '')
     let dataSplit = dataList.trim().split(" ")
-    console.log(dataSplit)
     console.log(dataSplit.length + " .cyc files found")
     for(i=0; i < dataSplit.length; i +=1 ){
-        //Use default readfile for Async > assign numbers to letters > store > shuffle in ascending 
         const stringData = fs.readFileSync("./" + dataSplit[i], 'utf8');
         stringArray.push(stringData)
     }
     console.log(stringArray.length + " .cyc Files Stringified")
+
+    // One For Each Chapter-Async
+    // Figure out a better solution for missing links (modify the onehtml rule file)
+    // The current solution still has working links for those inside the chapter and for those outside, nothing happens
+    for(i=0; i < stringArray.length; i +=1 ){
+        let chapterHTML = cyc(stringArray[i], onehtml)
+        let slicedSplit = dataSplit[i].slice(0,-4)
+        let modChapterHTML = chapterHTML.replace(/MISSING LINK/g, '')
+        fs.writeFile(slicedSplit + '.html', modChapterHTML, (err) => {
+            if (err) throw err;
+            console.log(slicedSplit + '.html has been saved!');
+          });
+    }
+    
+    //HTML Single File Processor
     stringCombined = stringArray.join()
     let stringCyc = stringCombined.replace(/,@+/g, " @")
-    //HTML Single File Processor
     let stringHTML = cyc(stringCyc, onehtml)
-    fs.writeFileSync('book.html', stringHTML, 'utf8');
+    fs.writeFile('book.html', stringHTML, (err) => {
+        if (err) throw err;
+        console.log('book.html has been saved!');
+      });
+    
     //Add Cyc Processors Here
 }) 
 
